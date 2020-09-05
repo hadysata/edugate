7d7aa19f722b01ed2215bb66c297c87eb2727351
Hady <hadysata@gmail.com>
2020-09-05 07:50:42 +0300

    Improve performance

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import requests
+from requests import Session
 import re
 from bs4 import BeautifulSoup
 from time import sleep
 import json
 from pathlib import Path
+from time import gmtime, strftime
 
 target = "https://edugate.ksu.edu.sa/ksu/ui/guest/timetable/index/scheduleTreeCoursesIndex.faces"
+
+session = Session()
 
 cookie = ""
 
 mainPage = ""
 
-univerityCourses = []
+filesPath = "/"
 
+univerityCourses = []
+univerityMajors = []
 formOptions = []
 hiddenValues = []
 
@@ -30,17 +35,16 @@ def cookiesToString(cookie):
 
 
 def get(targetUrl=target, payload=[], attempt=1):
-    requestData = None
     try:
-        requestData = requests.post(target, data=payload, headers=headers())
+        return session.post(target, data=payload)
     except:
         print("Error")
         if(attempt < 3):
             sleep(attempt * 5)
-            get(targetUrl, payload)
+            attempt +=1
+            return get(targetUrl, payload , attempt)
         else:
-            return 0
-    return requestData
+             0
 
 
 def getCookies(page):
@@ -55,7 +59,7 @@ def setCookies(cookies):
 def setUpOptions():
     page = get()
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'lxml')
 
     options = soup.findAll('select')
 
@@ -64,7 +68,7 @@ def setUpOptions():
         optionsListValues = []
         optionId = option.attrs['id']
         optionsList = BeautifulSoup(
-            str(option), 'html.parser').find_all('option')
+            str(option), 'lxml').find_all('option')
         for o in optionsList:
             optionsListValues.append(o['value'])
         result = {
@@ -83,10 +87,10 @@ def removeDuplicate(inputList):
 def setUpHiddenValues(page):
     global hiddenValues
     global mainPage
-    
+
     mainPage = page
-    
-    soup = BeautifulSoup(page.content, 'html.parser')
+
+    soup = BeautifulSoup(page.content, 'lxml')
 
     hidden_tags = soup.findAll("input", type="hidden")
 
@@ -96,20 +100,18 @@ def setUpHiddenValues(page):
         except:
             next
 
-
-
     hiddenValues = removeDuplicate(hiddenValues)
-
 
 
 def getMajors(select1, select2):
     global hiddenValues
+    global univerityMajors
 
     params = hiddenValues[:]
     params.remove(('myForm:index', ''))
     params.append(('myForm:select1', select1))
     params.append(('myForm:select2', select2))
-    params.append(('myForm:_idcl' , ''))
+    params.append(('myForm:_idcl', ''))
 
     page = get(payload=params)
 
@@ -125,13 +127,17 @@ def getMajors(select1, select2):
                 majorCode = majorDetailsSplitter[0]
                 majorName = majorDetailsSplitter[1]
             else:
-                majorCode = majorDetailsSplitter[0]
+                majorCode = "{}_{}".format(majorDetailsSplitter[0] , majorDetailsSplitter[1])
                 majorName = majorDetailsSplitter[2]
 
             majorIndex = major.split("javascript:setIndex(")[1].split(")")[0]
 
             majorsList.append(
                 {"code": majorCode, "name": majorName, "index": majorIndex})
+            
+            majorData = {'code': majorCode , 'name': majorName}
+            univerityMajors.append(majorData)
+            autoSave(majorData , 'majors')
 
     return removeDuplicate(majorsList)
 
@@ -151,25 +157,46 @@ def getMajorCourses(majorIndex, select1, select2):
     else:
         params.append(('myForm:commandLink', 'myForm:commandLink'))
 
-    page = get(payload=params)
+    try:
+     page = get(payload=params)
+ 
+     soup = BeautifulSoup(page.content, 'lxml')
+ 
+     rows = soup.findAll('tr', {"class": re.compile('^ROW')})
+ 
+     for row in rows:
+         data = row.text.splitlines()
+         coursesList.append(
+             {'code': data[1], 'name': data[2], 'credits': toFloat(data[5])  , 'gender': getGender(data[6]) , 'type': data[4]})
+ 
+     return removeDuplicate(coursesList)
+    except:
+     print("Error downloading a major courses")
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+def getGender(data):
+    return 'male' if 'Male' in data or 'ذكر' in data else 'female' if 'Female' in data or 'أنثى' in data else 'other'
 
-    rows = soup.findAll('tr', {"class": re.compile('^ROW')})
-
-    for row in rows:
-        data = row.text.splitlines()
-        coursesList.append(
-            {'code': data[1], 'name': data[2], 'credits': data[5]})
-
-    return removeDuplicate(coursesList)
-
+def toFloat(value):
+  try:
+    value = float(value)
+    return value
+  except ValueError:
+    return None
 
 def init():
+    global filesPath
+
+    filesPath = newFolder()
+
+    session.headers.update(headers())    
+
     page = get()
     getCookies(page)
     setUpOptions()
     setUpHiddenValues(page)
+
+    session.headers.update(headers())    
+
 
 
 def worker():
@@ -189,32 +216,47 @@ def worker():
         for select2 in select2List:
             majors = getMajors(select1, select2)
             for major in majors:
-                univerityCourses.extend(downloader(major, select1, select2))
-                autoSave()
+                try:
+                 data = downloader(major, select1, select2)
+                 univerityCourses.extend(data)
+                 autoSave(data=data, fileName='courses')
+                except:
+                    next
 
-    univerityCourses = removeDuplicate(univerityCourses)
     print('Total of {} courses found in this target'.format(len(univerityCourses)))
-    save()
+    print('Total of {} majors found in this target'.format(len(univerityMajors)))
+    save(data=univerityCourses, fileName='courses')
+    save(data=univerityMajors, fileName='majors')
 
 
 def downloader(major, select1, select2):
-    print('downloading {} Courses...'.format(major['code']))
-    coursesList = getMajorCourses(major['index'], select1, select2)
-    print('{} courses found in {}'.format(len(coursesList), major['code']))
-    return coursesList
+    try:
+     print('downloading {} Courses...'.format(major['code']))
+     coursesList = getMajorCourses(major['index'], select1, select2)
+     print('{} courses found in {}'.format(len(coursesList), major['code']))
+     return coursesList
+    except:
+        print("Error with downloader")
 
 
-def save():
+def save(data, fileName):
     global univerityCourses
-    with open('data.json', 'w+', encoding='utf-8') as f:
-        json.dump(univerityCourses, f, ensure_ascii=False)
+    with open('{}/{}.json'.format(filesPath, fileName), 'w+', encoding='utf-8') as f:
+        json.dump(removeDuplicate(data), f, ensure_ascii=False)
 
 
-def autoSave():
-    global univerityCourses
-    with open('data.json', 'a+', encoding='utf-8') as f:
-        json.dump(univerityCourses, f, ensure_ascii=False)
+def autoSave(data, fileName):
+    with open('{}/{}.json'.format(filesPath, fileName), 'a+', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False)
 
+
+def newFolder():
+    global target
+    folderName = strftime("%Y|%m|%d %H-%M-%S", gmtime())
+    targetName = target.split('.')[1]
+    path = "{}/{}".format(folderName, targetName)
+    Path(path).mkdir(parents=True, exist_ok=True)
+    return path
 
 
 init()
